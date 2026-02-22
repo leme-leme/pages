c5f968f58c9a5014478aa06f5d15a99146749499
Leander Jansen <leander@leme.nl>
2026-02-22 23:50:00 +0100

    chore: convert drizzle config to SQLite

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
-import "@/db/envConfig";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "postgresql",
+  dialect: "sqlite",
   schema: "./db/schema.ts",
   out: "./db/migrations",
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: process.env.DATABASE_URL || "file:./local.db"
   }
 });
